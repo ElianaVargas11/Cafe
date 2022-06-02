3499daf481a25881339e083236ece7ee7eeda351
ElianaVargas11 <evargasrojas11@gmail.com>
2022-06-02 08:52:55 -0400

    Subiendo archivo

---
+++ FrontendCafeAndre/src/app/Componentes/adminreportes/adminreportes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adminreportes',
+  templateUrl: './adminreportes.component.html',
+  styleUrls: ['./adminreportes.component.css']
+})
+export class AdminreportesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ FrontendCafeAndre/src/app/Componentes/cajeroreporteventas/cajeroreporteventas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-cajeroreporteventas',
+  templateUrl: './cajeroreporteventas.component.html',
+  styleUrls: ['./cajeroreporteventas.component.css']
+})
+export class CajeroreporteventasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {    
+  }
+
+}
